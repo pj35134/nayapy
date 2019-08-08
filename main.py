4862eb72cb8677ff889437cc151b79b5f474afd7
Prashant Tandan <pracant.tandon@gmail.com>
2019-08-08 11:52:27 +0545

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
-@app.route('/')
-def upload_form():
-	return render_template('upload.html')
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -22,14 +18,11 @@ def upload_file():
 			flash('No file part')
 			return redirect(request.url)
 		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			flash('File successfully uploaded')
-			return redirect('/')
+			return 10
 		else:
 			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
 			return redirect(request.url)
