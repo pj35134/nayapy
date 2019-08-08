babef8a7376e5baece01a064f4233d595ab06758
pj35134 <pj35134@gmail.com>
2019-08-08 19:45:49 +0545

    refreshed

--- main.py
+++ main.py
@@ -2,29 +2,40 @@ import os
 #import magic
 import urllib.request
 from app import app
-from flask import Flask, flash, request, redirect, render_template
+global filename
+from flask import Flask, flash, request, redirect, render_template,Response,jsonify, make_response
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	print('In upload form')
+	print('filename=',filename)
+	return '5'
 
 @app.route('/', methods=['POST'])
 def upload_file():
+	print('in upload file')
 	if request.method == 'POST':
-        # check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
 		file = request.files['file']
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return 10
+			print('File successfully uploaded')
+			print('In upload form')
+			print('filename=',filename)
+			return '20'
+			#return redirect('/')
+			#return make_response('10')
+			#res = make_response('10')
+			#res.headers['message'] = 'NOt Okay!'
+			#return res
 		else:
-			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+			print('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
 			return redirect(request.url)
 
 if __name__ == "__main__":
